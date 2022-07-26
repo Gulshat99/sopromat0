bb84d3b8abba59306f5cde65cf8fbe60a9edd732
Gulshat99 <110035146+Gulshat99@users.noreply.github.com>
2022-07-26 17:46:54 +0500

    Add files via upload

---
+++ 33lr.js
@@ -0,0 +1,746 @@
+ var img1=document.createElement("img");
+    img1.setAttribute("src","./0aa0.png");
+    img1.setAttribute("width","100");
+
+ var img2=document.createElement("img");
+    img2.setAttribute("src","./kk001.png");
+    img2.setAttribute("width","100");
+
+ var img3=document.createElement("img");
+    img3.setAttribute("src","./re054.png");
+    img3.setAttribute("width","100");
+
+ var img4=document.createElement("img");
+    img4.setAttribute("src","./tr1.png");
+    img4.setAttribute("width","100");
+
+ var img5=document.createElement("img");
+    img5.setAttribute("src","./re0071.png");
+    img5.setAttribute("width","100");
+
+ var img6=document.createElement("img");
+    img6.setAttribute("src","./re0061.png");
+    img6.setAttribute("width","100");
+
+var img7=document.createElement("img");
+    img7.setAttribute("src","./0qs1.png");
+    img7.setAttribute("width","100");
+
+var img8=document.createElement("img");
+    img8.setAttribute("src","./m2.png");
+    img8.setAttribute("width","100");
+
+var img9=document.createElement("img");
+    img9.setAttribute("src","./hh09.png");
+    img9.setAttribute("width","100");
+
+var img10=document.createElement("img");
+    img10.setAttribute("src","./io.png");
+    img10.setAttribute("width","100");
+
+var img11=document.createElement("img");
+    img11.setAttribute("src","./ii.png");
+    img11.setAttribute("width","100");
+
+var img12=document.createElement("img");
+    img12.setAttribute("src","./3j.png");
+    img12.setAttribute("width","100");
+
+var img13=document.createElement("img");
+    img13.setAttribute("src","./ui998.png");
+    img13.setAttribute("width","100");
+
+var img14=document.createElement("img");
+    img14.setAttribute("src","./as109.png");
+    img14.setAttribute("width","100");
+
+var img15=document.createElement("img");
+    img15.setAttribute("src","./zzq2.png");
+    img15.setAttribute("width","100");
+
+var img16=document.createElement("img");
+    img16.setAttribute("src","./91.png");
+    img16.setAttribute("width","100");
+
+var img17=document.createElement("img");
+    img17.setAttribute("src","./re0081.png");
+    img17.setAttribute("width","100");
+
+var img18=document.createElement("img");
+    img18.setAttribute("src","./92.png");
+    img18.setAttribute("width","100");
+
+var img19=document.createElement("img");
+    img19.setAttribute("src","./g09.png");
+    img19.setAttribute("width","100");
+
+var img20=document.createElement("img");
+    img20.setAttribute("src","./ui9919.png");
+    img20.setAttribute("width","100");
+
+var Gost_D = new Array()
+
+Gost_D[1] = 1;
+Gost_D[2] = 1.1;
+Gost_D[3] = 1.2;
+Gost_D[4] = 1.3;
+Gost_D[5] = 1.4;
+Gost_D[6] = 1.5;
+Gost_D[7] = 1.6;
+Gost_D[8] = 1.7;
+Gost_D[9] = 1.8;
+Gost_D[10] = 1.9;
+Gost_D[11] = 1.95;
+Gost_D[12] = 2;
+Gost_D[13] = 2.05;
+Gost_D[14] = 2.1;
+Gost_D[15] = 2.15;
+Gost_D[16] = 2.20;
+Gost_D[17] = 2.25;
+Gost_D[18] = 2.30;
+Gost_D[19] = 2.35;
+Gost_D[20] = 2.40;
+Gost_D[21] = 2.45;
+Gost_D[22] = 2.50;
+Gost_D[23] = 2.55;
+Gost_D[24] = 2.60;
+Gost_D[25] = 2.65;
+Gost_D[26] = 2.70;
+Gost_D[27] = 2.75;
+Gost_D[28] = 2.80;
+Gost_D[29] = 2.85;
+Gost_D[30] = 2.90;
+Gost_D[31] = 2.95;
+Gost_D[32] = 3;
+Gost_D[33] = 3.10;
+Gost_D[34] = 3.15;
+Gost_D[35] = 3.20;
+Gost_D[36] = 3.30;
+Gost_D[37] = 3.35;
+Gost_D[38] = 3.40;
+Gost_D[39] = 3.50;
+Gost_D[40] = 3.60;
+Gost_D[41] = 3.70;
+Gost_D[42] = 3.80;
+Gost_D[43] = 3.90;
+Gost_D[44] = 4;
+Gost_D[45] = 4.10;
+Gost_D[46] = 4.20;
+Gost_D[47] = 4.25;
+Gost_D[48] = 4.30;
+Gost_D[49] = 4.40;
+Gost_D[50] = 4.50;
+Gost_D[51] = 4.60;
+Gost_D[52] = 4.70;
+Gost_D[53] = 4.80;
+Gost_D[54] = 4.90;
+Gost_D[55] = 5;
+Gost_D[56] = 5.10;
+Gost_D[57] = 5.20;
+Gost_D[58] = 5.30;
+Gost_D[59] = 5.40;
+Gost_D[60] = 5.50;
+Gost_D[61] = 5.60;
+Gost_D[62] = 5.70;
+Gost_D[63] = 5.80;
+Gost_D[64] = 5.90;
+Gost_D[65] = 6;
+Gost_D[66] = 6.10;
+Gost_D[67] = 6.20;
+Gost_D[68] = 6.30;
+Gost_D[69] = 6.40;
+Gost_D[70] = 6.50;
+Gost_D[71] = 6.60;
+Gost_D[72] = 6.70;
+Gost_D[73] = 6.80;
+Gost_D[74] = 6.90;
+Gost_D[75] = 7;
+Gost_D[76] = 7.10;
+Gost_D[77] = 7.20;
+Gost_D[78] = 7.30;
+Gost_D[79] = 7.40;
+Gost_D[80] = 7.50;
+Gost_D[81] = 7.60;
+Gost_D[82] = 7.70;
+Gost_D[83] = 7.80;
+Gost_D[84] = 7.90;
+Gost_D[85] = 8;
+Gost_D[86] = 8.10;
+Gost_D[87] = 8.20;
+Gost_D[88] = 8.30;
+Gost_D[89] = 8.40;
+Gost_D[90] = 8.50;
+Gost_D[91] = 8.60;
+Gost_D[92] = 8.70;
+Gost_D[93] = 8.80;
+Gost_D[94] = 8.90;
+Gost_D[95] = 9;
+Gost_D[96] = 9.10;
+Gost_D[97] = 9.20;
+Gost_D[98] = 9.30;
+Gost_D[99] = 9.40;
+Gost_D[100] =9.50;
+Gost_D[101] =9.60;
+Gost_D[102] = 9.70;
+Gost_D[103] = 9.80;
+Gost_D[104] = 9.90;
+Gost_D[105] = 10;
+Gost_D[106] = 10.10;
+Gost_D[107] = 10.20;
+Gost_D[108] = 10.30;
+Gost_D[109] = 10.40;
+Gost_D[110] =10.50;
+Gost_D[111] =10.60;
+Gost_D[112] = 10.70;
+Gost_D[113] = 10.80;
+Gost_D[114] = 10.90;
+Gost_D[115] = 11;
+Gost_D[116] = 11.10;
+Gost_D[117] = 11.20;
+Gost_D[118] = 11.30;
+Gost_D[119] = 11.40;
+Gost_D[120] =11.50;
+Gost_D[121] =11.60;
+Gost_D[122] = 11.70;
+Gost_D[123] = 11.80;
+Gost_D[124] = 11.90;
+Gost_D[125] = 12;
+Gost_D[126] = 12.10;
+Gost_D[127] = 12.20;
+Gost_D[128] = 12.30;
+Gost_D[129] = 12.40;
+Gost_D[130] =12.50;
+Gost_D[131] =12.60;
+Gost_D[132] = 12.70;
+Gost_D[133] = 12.80;
+Gost_D[134] = 12.90;
+Gost_D[135] = 13;
+Gost_D[136] = 13.10;
+Gost_D[137] = 13.20;
+Gost_D[138] = 13.30;
+Gost_D[139] = 13.40;
+Gost_D[140] =13.50;
+Gost_D[141] =13.60;
+Gost_D[142] = 13.70;
+Gost_D[143] = 13.75;
+Gost_D[144] = 13.80;
+Gost_D[145] = 13.90;
+Gost_D[146] = 14.00;
+Gost_D[147] = 14.25;
+Gost_D[148] = 14.50;
+Gost_D[149] = 14.75;
+Gost_D[150] =15.00;
+Gost_D[151] =15.25;
+Gost_D[152] = 15.40;
+Gost_D[153] = 15.50;
+Gost_D[154] = 15.75;
+Gost_D[155] = 16;
+Gost_D[156] = 16.25;
+Gost_D[157] = 16.50;
+Gost_D[158] = 16.75;
+Gost_D[159] = 17.0;
+Gost_D[160] =17.25;
+Gost_D[161] =17.40;
+Gost_D[162] = 17.50;
+Gost_D[163] = 17.75;
+Gost_D[164] = 18.00;
+Gost_D[165] = 18.25;
+Gost_D[166] = 18.50;
+Gost_D[167] = 18.75;
+Gost_D[168] = 19.00;
+Gost_D[169] = 19.25;
+Gost_D[170] =19.40;
+Gost_D[171] =19.50;
+Gost_D[172] = 19.75;
+Gost_D[173] = 20.00;
+Gost_D[174] = 20.25;
+Gost_D[175] = 20.50;
+Gost_D[176] = 20.75;
+Gost_D[177] = 21.00;
+Gost_D[178] = 21.25;
+Gost_D[179] = 21.50;
+Gost_D[180] =21.75;
+Gost_D[181] =22.00;
+Gost_D[182] = 22.50;
+Gost_D[183] = 22.75;
+Gost_D[184] = 23.00;
+Gost_D[185] = 23.25;
+Gost_D[186] = 23.50;
+Gost_D[187] = 23.75;
+Gost_D[188] = 24.00;
+Gost_D[189] = 24.25;
+Gost_D[190] = 24.50;
+Gost_D[191] = 24.75;
+Gost_D[192] = 25.00;
+Gost_D[193] = 25.25;
+Gost_D[194] = 25.50;
+Gost_D[195] = 25.75;
+Gost_D[196] = 26.00;
+Gost_D[197] = 26.25;
+Gost_D[198] = 26.50;
+Gost_D[199] = 26.75;
+Gost_D[200] = 27.00;
+Gost_D[201] = 27.25;
+Gost_D[202] = 27.50;
+Gost_D[203] = 27.75;
+Gost_D[204] = 28.00;
+Gost_D[205] = 28.25;
+Gost_D[206] = 28.50;
+Gost_D[207] = 28.75;
+Gost_D[208] = 29.00;
+Gost_D[209] = 29.25;
+Gost_D[210] = 29.50;
+Gost_D[211] = 29.75;
+Gost_D[212] = 30.00;
+Gost_D[213] = 30.25;
+Gost_D[214] = 30.50;
+Gost_D[215] = 30.75;
+Gost_D[216] = 31.00;
+Gost_D[217] = 31.25;
+Gost_D[218] = 31.50;
+
+
+var Gost_q = new Array()
+Gost_q[1] = 0.40;
+Gost_q[2] = 0.45;
+Gost_q[3] = 0.50;
+Gost_q[4] = 0.55;
+Gost_q[5] = 0.60;
+Gost_q[6] = 0.63;
+Gost_q[7] = 0.65;
+Gost_q[8] = 0.70;
+Gost_q[9] = 0.75;
+Gost_q[10] = 0.80;
+Gost_q[11] = 0.90;
+Gost_q[12] = 1.00;
+Gost_q[13] = 1.20;
+Gost_q[14] = 1.30;
+Gost_q[15] = 1.40;
+Gost_q[16] = 1.5;
+Gost_q[17] = 1.6;
+Gost_q[18] = 1.8;
+Gost_q[19] = 2.0;
+Gost_q[20] = 2.2;
+Gost_q[21] = 2.5;
+Gost_q[22] = 2.8;
+Gost_q[23] = 3.0;
+Gost_q[24] = 3.2;
+Gost_q[25] = 3.5;
+Gost_q[26] = 3.8;
+Gost_q[27] = 3.9;
+Gost_q[28] = 4.0;
+Gost_q[29] = 4.5;
+Gost_q[30] = 5.0;
+Gost_q[31] = 5.5;
+Gost_q[32] = 6.0;
+Gost_q[33] = 6.5;
+Gost_q[34] = 7.0;
+Gost_q[35] = 7.5;
+Gost_q[36] = 8.0;
+Gost_q[37] = 8.5;
+Gost_q[38] = 9.0;
+Gost_q[39] = 9.5;
+Gost_q[40] = 10.0;
+Gost_q[41] = 10.5;
+Gost_q[42] = 11.0;
+Gost_q[43] = 11.5;
+Gost_q[44] = 12.0;
+Gost_q[45] = 13.0;
+Gost_q[46] = 13.5;
+Gost_q[47] = 14.0;
+Gost_q[48] = 14.5;
+Gost_q[49] = 15.0;
+Gost_q[50] = 15.5;
+Gost_q[51] = 16.0;
+Gost_q[52] = 16.5;
+Gost_q[53] = 17.0;
+Gost_q[54] = 17.5;
+Gost_q[55] = 18.0;
+Gost_q[56] = 18.5;
+Gost_q[57] = 19.0;
+Gost_q[58] = 19.5;
+Gost_q[59] = 20.0;
+Gost_q[60] = 20.5;
+Gost_q[61] = 21.0;
+Gost_q[62] = 21.5;
+Gost_q[63] = 22.0;
+Gost_q[64] = 22.5;
+Gost_q[65] = 23.0;
+Gost_q[66] = 23.5;
+Gost_q[67] = 24.0;
+Gost_q[68] = 24.5;
+Gost_q[69] = 25.0;
+Gost_q[70] = 25.5;
+Gost_q[71] = 26.0;
+Gost_q[72] = 27.0;
+Gost_q[73] = 28.0;
+Gost_q[74] = 29.0;
+Gost_q[75] = 30.0;
+Gost_q[76] = 31.0;
+Gost_q[77] = 32.0;
+Gost_q[78] = 34.0;
+Gost_q[79] = 36.0;
+Gost_q[80] = 38.0;
+Gost_q[81] = 40.0;
+Gost_q[82] = 42.0;
+Gost_q[83] = 45.0;
+Gost_q[84] = 48.0;
+Gost_q[85] = 50.0;
+Gost_q[86] = 52.0;
+Gost_q[87] = 55.0;
+Gost_q[88] = 58.0;
+Gost_q[89] = 60.0;
+Gost_q[90] = 62.0;
+Gost_q[91] = 65.0;
+Gost_q[92] = 68.0;
+Gost_q[93] = 70.0;
+Gost_q[94] = 72.0;
+Gost_q[95] = 75.0;
+Gost_q[96] = 78.0;
+Gost_q[97] = 80.0;
+Gost_q[98] = 82.0;
+Gost_q[99] = 85.0;
+Gost_q[100] = 87.0;
+Gost_q[101] = 90.0;
+Gost_q[102] = 92.0;
+Gost_q[103] = 95.0;
+Gost_q[104] = 100.0;
+Gost_q[105] = 105.0;
+Gost_q[106] = 110.0;
+Gost_q[107] = 115.0;
+Gost_q[108] = 120.0;
+Gost_q[109] = 125.0;
+Gost_q[110] = 130.0;
+Gost_q[111] = 135.0;
+Gost_q[112] = 140.0;
+Gost_q[113] = 150.0;
+Gost_q[114] = 155.0;
+Gost_q[115] = 160.0;
+
+
+
+var t;
+function up() {
+	var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+	if(top > 0) {
+		window.scrollBy(0,-100);
+		t = setTimeout('up()',20);
+	} else clearTimeout(t);
+	return false;
+}
+function zad1(){
+
+    var ver_z = document.getElementById("ver_z").value;
+    var gor_z = document.getElementById("gor_z").value;
+    var zad1_F = document.getElementById("zad1_F").value;
+    var zad1_i = document.getElementById("zad1_i").value;
+    bolt.ctx.clearRect(0 , 0, bolt.cnvs.width, bolt.cnvs.height);
+    var zad1_tcr;
+    var zad1_qcm;
+    var zadMv = document.getElementById("zadMv").value;
+     if (zadMv=="Ст0")
+    {
+        zad1_tcr=140;
+        zad1_qcm=280;
+    }
+    if (zadMv=="Ст2")
+    {
+        zad1_tcr=140;
+        zad1_qcm=280;
+    }
+    if (zadMv=="Ст3")
+    {
+       zad1_tcr=140;
+        zad1_qcm=320;
+    }
+    var w=zad1_i;
+    var l;
+    if (w==1)
+        {
+            bolt.draw_image(img1,(x2-x1)/4.65-100,115);
+            bolt.draw_image(img19,(x2-x1)/4.2-100,96);
+        }
+    else if (w==2)
+        {
+            bolt.draw_image(img7,(x2-x1)/4.1-100,102);
+            bolt.draw_image(img16,(x2-x1)/5.4-100,86);
+            bolt.draw_image(img18,(x2-x1)/5.4-100,133);
+        }
+    var q=ver_z;
+    if (q==4)
+        {
+            bolt.draw_image(img17,(x2-x1)/4.8-100,225); 
+        }
+    if (q==2)
+        {
+            bolt.draw_image(img6,(x2-x1)/5-100,230);
+        }
+    if (q==3)
+        {
+            bolt.draw_image(img5,(x2-x1)/4.8-100,225); 
+        }
+    else if (q<=1) 
+        {
+         bolt.draw_image(img3,(x2-x1)/5.2-100,232);         
+        }
+
+   
+    var zad1_z= (ver_z*gor_z);
+    
+    var i=0;
+    var j=0;
+    let arr = [];
+    
+    while(i<10)
+        {
+        
+        i+=1;
+        if (i % 2 ==0)
+            {
+                if (w==1)
+                    {
+                        bolt.draw_image(img10,(x2-x1)/1.96-i*25,92);
+                    } 
+                else if (w==2) 
+                    {
+                        bolt.draw_image(img12,(x2-x1)/1.93-i*25,92);
+                    }
+                  for (j=0; j<ver_z;j++)
+                    {
+                        bolt.draw_image(img4,(x2-x1)/2-i*25,240+j*47);
+                    } 
+                
+
+            }
+        else if (i % 2!=0)
+            {
+                if (w==1)
+                {
+                    bolt.draw_image(img10,(x2-x1)/1.95-25+i*25,92);
+                }
+                else if (w==2)
+                {
+                    bolt.draw_image(img12,(x2-x1)/1.92-25+i*25,92);
+                    
+                }
+                
+                for (j=0; j<ver_z;j++){
+                bolt.draw_image(img4,(x2-x1)/2-25+i*25,240+j*47); 
+            }
+            }
+        if (gor_z==i)
+            {
+            break;
+            }
+
+        
+    }
+    
+    
+
+    
+    var d1 =Math.sqrt((4*zad1_F*1000)/(Math.PI*zad1_z*zad1_i*zad1_tcr)).toFixed(2);
+    let a=0;
+    while (a<219){
+        a++;
+        if (d1<Gost_D[a])
+            {
+                d1=Gost_D[a];
+
+                var b = (zad1_F*1000/(d1*zad1_qcm*zad1_z)).toFixed(2);
+                bolt.draw_text(650, 0, 575,"Номинальный диаметр определен согласно ГОСТ 886-77 = "+ d1+" мм", 'black', 18);
+                break;
+            }  
+    }
+    let k=0;
+    while (k<117){
+        k++;
+        if (b<Gost_q[k])
+            {
+                b=Gost_q[k];
+                bolt.draw_text(650, 0, 625,"Согласно ГОСТ 19903-2015 толщина пластины  = "+ b+" мм", 'black', 18);
+                break;
+            }  
+    }
+    if (w==2){
+    var bb = (b/2).toFixed(2);
+    let r=0;
+    while (r<117){
+        r++;
+        if (bb<Gost_q[r])
+            {
+                bb=Gost_q[r];
+                bolt.draw_text(650, 0, 675,"Согласно ГОСТ 19903-2015 толщина крайних пластин  = "+ bb+" мм", 'black', 18);
+                break;
+            }  
+    }
+}
+    
+
+}
+
+
+
+function zad2()
+    {
+    var ver2_z = document.getElementById("ver2_z").value;
+    var gor2_z = document.getElementById("gor2_z").value;
+    var zad2_d = document.getElementById("zad2_d").value;
+    var zad2_i = document.getElementById("zad2_i").value;
+    var zad2_z= (ver2_z*gor2_z);
+    bolt.ctx.clearRect(0 , 0, bolt.cnvs.width, bolt.cnvs.height);
+    var l = zad2_i;
+    var s = ver2_z;
+    var zad2_tcr;
+    var zad2_qcm;
+    var zadMv2 = document.getElementById("zadMv2").value;
+     if (zadMv2=="Ст0")
+    {
+        zad2_tcr=140;
+        zad2_qcm=280;
+    }
+    if (zadMv2=="Ст2")
+    {
+        zad2_tcr=140;
+        zad2_qcm=280;
+    }
+    if (zadMv2=="Ст3")
+    {
+       zad2_tcr=140;
+        zad2_qcm=320;
+    }
+    if (l==1)
+        {
+            bolt.draw_image(img9,(x2-x1)/4.6-100,103);
+        }
+    else if (l==2)
+        {
+            bolt.draw_image(img11,(x2-x1)/4.7-100,85);
+        }
+    if (s==1)
+        {
+            bolt.draw_image(img15,(x2-x1)/4.5-100,252);
+        }
+    if (s==2)
+        {
+            bolt.draw_image(img13,(x2-x1)/4.7-100,245);
+        }
+    if (s==3)
+    	{
+    		bolt.draw_image(img20,(x2-x1)/4.7-100,241);
+    	}
+    else if (s>3)
+        {
+            bolt.draw_image(img14,(x2-x1)/4.7-100,250);
+        }
+    var i =0;
+    var j=0;
+
+    for (i=0; i<gor2_z;i++)
+        {
+            if (l==1)
+                {
+                    bolt.draw_image(img10,(x2-x1)/1.91-45-i*55,88);
+                    bolt.draw_image(img10,(x2-x1)/1.72+i*55,88);
+                }
+            else if (l==2)
+                {
+                    bolt.draw_image(img12,(x2-x1)/1.87-45-i*55,89);
+                    bolt.draw_image(img12,(x2-x1)/1.7+i*55,89);
+                }
+         for (j=0; j<ver2_z;j++){
+                bolt.draw_image(img4,(x2-x1)/2.08-25-i*55,265+j*47);
+                bolt.draw_image(img4,(x2-x1)/1.9+25+i*55,265+j*47);
+            }
+        }
+   
+
+    
+   
+var F =(zad2_tcr*zad2_i*zad2_z*(Math.PI*zad2_d*zad2_d)/4000).toFixed(2);
+ var b = (F*1000/(zad2_d*zad2_qcm*zad2_z)).toFixed(2);
+    bolt.draw_text(700, 0, 575,"Минимальная сила = "+ F+" кН", 'black', 18);
+   
+    let k=0;
+    while (k<117){
+        k++;
+        if (b<Gost_q[k])
+            {
+                b=Gost_q[k];
+                bolt.draw_text(650, 0, 625,"Согласно ГОСТ 19903-2015 толщина пластины  = "+ b+" мм", 'black', 18);
+                break;
+            }  
+    }
+    if (l==2){
+    var bb= (b/2).toFixed(2);
+    let r=0;
+    while (r<117){
+        r++;
+        if (bb<Gost_q[r])
+            {
+                bb=Gost_q[r];
+                bolt.draw_text(645, 0, 675,"Согласно ГОСТ 19903-2015 толщина крайних пластин  = "+ bb+" мм", 'black', 18);
+                break;
+            }  
+    }
+}
+
+}
+
+
+
+document.getElementById('zad1').onclick = () => {
+    zad1();
+    up();
+}
+document.getElementById("zad1_F").oninput= () => zad1();
+
+
+
+document.getElementById('zad2').onclick = () => {
+    zad2();
+    up();
+}
+document.getElementById("zad2_d").oninput= () => zad2();
+
+
+
+
+function setupCanvas(canvas) {
+    var dpr = window.devicePixelRatio || 1;
+    var rect = canvas.getBoundingClientRect();
+    canvas.width = rect.width * dpr;
+    canvas.height = rect.height * dpr;
+    var ctx = canvas.getContext('2d');
+    ctx.scale(dpr, dpr);
+    return ctx;
+}
+
+var x1, x2;
+if(document.documentElement.clientWidth > 1000){
+    x1 = ~~(0.1 * document.documentElement.clientWidth / 2);
+    x2 = ~~(0.9 * document.documentElement.clientWidth / 2);
+}
+else{
+    x1 = ~~(0.1 * document.documentElement.clientWidth);
+    x2 = ~~(0.9 * document.documentElement.clientWidth);
+}
+class Bolt{
+    constructor(){
+        this.cnvs = document.getElementById('cnvs');
+        this.ctx = setupCanvas(this.cnvs);
+    }
+    draw_text(x2, x1, y, text, color = 'black', size = 14){
+        this.ctx.font = size.toString() + 'px Arial';
+        this.ctx.textAlign = "center";
+        this.ctx.fillStyle = color;     
+        this.ctx.fillText(text, (x2 + x1) / 2, y); 
+    }
+    draw_image(img, x, y){
+
+        this.ctx.drawImage(img, x, y);
+        } 
+    }
+var bolt = new Bolt();
+
+
